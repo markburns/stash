577fe37e1eb4e73eaa1c4459a5caf9c993853288
Mark Burns <markthedeveloper@gmail.com>
2011-08-11 21:45:50 +0100

    added new test for instantiating from a Ruby hash

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -5,25 +5,33 @@ describe Stash::Hash do
     @hash = Stash::Hash[:foobar]
     @hash.clear
   end
-  
+
   it "clears hashes" do
     @hash[:foo] = "a"
     @hash[:bar] = "b"
     @hash.clear
     @hash.to_hash.should == {}
   end
-  
+
   it "sets and gets strings from hashes" do
     @hash[:foo].should be_nil
     @hash[:foo] = "42"
     @hash[:foo].should == "42"
   end
-  
+
   it "casts to a Ruby hash" do
     @hash.should be_empty
     @hash['foo'] = 'a'
     @hash['bar'] = 'b'
-    
+
     @hash.to_hash.should == {'foo'=>'a', 'bar'=>'b'}
+  end
+
+  it "instantiates from a Ruby hash" do
+    ruby_hash = {:foo => 'bar', :baz => 'qux'}
+    @hash = Stash::Hash.new :test, ruby_hash
+
+    @hash[:foo].should == 'bar'
+    @hash[:baz].should == 'qux'
   end
 end
